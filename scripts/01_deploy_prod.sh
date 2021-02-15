99fcffcc497379d3f8a48942d755052c997c4149
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 21:42:22 +0100

    simple file

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -11,6 +11,7 @@ m_month=`date +'%b'`
 docker logs wordpress_cca_wordpress_1  >test  2>/dev/null ; cat test | grep "/${m_month}/"| grep '\- \-' | cut -d':' -f1> src/data.txt
 echo "" >> src/data.txt
 sed -i ':a;N;$!ba;s/\n/]\n/g ' src/data.txt
+LONGUEUR=50 
 awk '{printf("%-'${LONGUEUR}'s\n", $0) }' src/data.txt > src/data2.txt
 mv  src/data2.txt src/data.txt
 
