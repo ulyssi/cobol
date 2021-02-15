b956ff185fa2c6a5e495169e6308293e9e3d2721
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 16:02:16 +0100

    cobol 1

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 # Pull base image
 FROM debian:jessie
-
+RUN apt-get update
+RUN apt-get install -y open-cobol
--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -1,4 +1,6 @@
 #mkdir src
 
 #docker build
-#docker run --rm -it -v `pwd`/src:/oscobol/src:ro --name oscobol opensourcecobol/opensource-cobol
+#docker run --rm -it -v `pwd`/src:/oscobol/src:ro --name oscobol 
+docker build  . -t batch_cobol_run
+docker run  --rm -v `pwd`/src:/oscobol/src:ro --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/batch.cbl;cobcrun batch_1"
