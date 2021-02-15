d34af044a05d48c30b57fa4adaf675f2b5b132cd
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-16 00:04:20 +0100

    new prog

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -14,27 +14,27 @@ mv  src/data2.txt src/data.txt
 echo "===============================TRAITEMENT ET MISE EN FORME==================="
 echo " RUNNING COBOL"
 docker build  . -t batch_cobol_run
-docker run  --rm -v `pwd`/src:/oscobol/src:rw --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/BATCH_1.cbl;cobcrun BATCH_1"
+docker run  --rm -v `pwd`/src:/oscobol/src:rw --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/BATCH_2.cbl;cobcrun BATCH_2"
 
 echo "===============================EXPOSE DATA==================="
 
 
-rm expose/.htpasswd
-mkdir expose
+# rm expose/.htpasswd
+# mkdir expose
 
-cp -rv src expose
+# cp -rv src expose
 
-docker stop  nginx_expose_data
-docker rm nginx_expose_data
+# docker stop  nginx_expose_data
+# docker rm nginx_expose_data
 
 
-docker run --name nginx_expose_data -p 8585:80  -v  `pwd`/expose:/usr/share/nginx/html -d nginx
-docker exec  nginx_expose_data bash -c 'sed  -i "s/index  index.html index.htm;/autoindex on;/g" /etc/nginx/conf.d/default.conf '
-docker exec  nginx_expose_data bash -c 'nginx -s reload '
+# docker run --name nginx_expose_data -p 8585:80  -v  `pwd`/expose:/usr/share/nginx/html -d nginx
+# docker exec  nginx_expose_data bash -c 'sed  -i "s/index  index.html index.htm;/autoindex on;/g" /etc/nginx/conf.d/default.conf '
+# docker exec  nginx_expose_data bash -c 'nginx -s reload '
 
-##Protection des données.
+# ##Protection des données.
 
-echo "tom:{SHA}loNd2L+nGL1kR8zIevia4Wddrso=" >  expose/.htpasswd
-docker exec  nginx_expose_data bash -c 'sed  -i "s/autoindex on;/autoindex on;auth_basic   \"closed site\";auth_basic_user_file \/usr\/share\/nginx\/html\/.htpasswd;/g" /etc/nginx/conf.d/default.conf '
-docker exec  nginx_expose_data bash -c 'sed  -i "s/location \/ {/location \/src {/g" /etc/nginx/conf.d/default.conf '
-docker exec  nginx_expose_data bash -c 'nginx -s reload '
+# echo "tom:{SHA}loNd2L+nGL1kR8zIevia4Wddrso=" >  expose/.htpasswd
+# docker exec  nginx_expose_data bash -c 'sed  -i "s/autoindex on;/autoindex on;auth_basic   \"closed site\";auth_basic_user_file \/usr\/share\/nginx\/html\/.htpasswd;/g" /etc/nginx/conf.d/default.conf '
+# docker exec  nginx_expose_data bash -c 'sed  -i "s/location \/ {/location \/src {/g" /etc/nginx/conf.d/default.conf '
+# docker exec  nginx_expose_data bash -c 'nginx -s reload '
---
+++ src/BATCH_2.cbl
@@ -0,0 +1,124 @@
+
+      ******************************************************************
+      *  Batch1 
+      *
+      ******************************************************************
+       IDENTIFICATION              DIVISION.
+      ******************************************************************
+       PROGRAM-ID.                 BATCH_1.
+       AUTHOR.                     UCA
+       DATE-WRITTEN.               20201502
+      ******************************************************************
+       ENVIRONMENT DIVISION. 
+      ******************************************************************
+       INPUT-OUTPUT SECTION.
+        FILE-CONTROL.
+           SELECT TRANSACTIONS ASSIGN TO '/oscobol/src/transactions.txt'
+           ORGANIZATION IS SEQUENTIAL.
+           SELECT VISIT_FILE ASSIGN TO '/oscobol/src/data.txt'
+           ORGANIZATION IS SEQUENTIAL.
+ 
+      ******************************************************************
+       DATA                        DIVISION.
+      ******************************************************************
+       FILE SECTION.
+       FD TRANSACTIONS.
+       01 OUTPUT_RECCORD     PIC X(50).
+       
+
+       FD VISIT_FILE.
+        01 INPUT-RECORD    PIC X(50).
+
+
+      ******************************************************************
+       WORKING-STORAGE             SECTION.
+      ******************************************************************
+       01 VISIT-STRUCT.
+         02 IP.
+           03 IP_1 PIC 9(3).
+           03 filler PIC X(1).
+           03 IP_2 PIC 9(3).
+           03 filler PIC X(1).
+           03 IP_3 PIC 9(3).
+           03 filler PIC X(1).
+           03 IP_4 PIC 9(3).
+         02 filler PIC X(5).
+         02 VISIT_DATE.
+           03 filler PIC X(1).
+           03 VDAY PIC X(2).
+           03 filler PIC X(1).
+           03 VMONTH PIC X(3).
+           03 filler PIC X(1).
+           03 VYEAR PIC X(4).
+           03 filler PIC X(1).
+ 
+       01 WS-CURRENT-DATE-DATA.
+        05  WS-CURRENT-DATE.
+           10  WS-CURRENT-YEAR         PIC 9(04).
+           10  WS-CURRENT-MONTH        PIC 9(02).
+           10  WS-CURRENT-DAY          PIC 9(02).
+       05  WS-CURRENT-TIME.
+           10  WS-CURRENT-HOURS        PIC 9(02).
+           10  WS-CURRENT-MINUTE       PIC 9(02).
+           10  WS-CURRENT-SECOND       PIC 9(02).
+           10  WS-CURRENT-MILLISECONDS PIC 9(02).
+
+       77 END-OF-FILE PIC Z(1). 
+      
+
+
+       PROCEDURE                   DIVISION.
+      ******************************************************************
+       MAIN-RTN.
+           DISPLAY "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!".
+           DISPLAY "! BATCH 1   :                                    !".
+           DISPLAY "! TRAITEMENT BACH STATISTIQUES VISITES           !".
+           DISPLAY "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!". 
+          
+       MAIN-EXT.
+           PERFORM 000-TRT-FONC001             
+              THRU 000-TRT-FONC001-FIN
+           STOP RUN.
+
+
+        000-TRT-FONC001.           
+           MOVE FUNCTION CURRENT-DATE TO WS-CURRENT-DATE-DATA
+           DISPLAY "PARAGRAPHE TRAITEMENT 1".
+           DISPLAY "CURRENT DATE " SPACE WS-CURRENT-DATE-DATA.
+           OPEN INPUT VISIT_FILE 
+           READ VISIT_FILE
+             AT END MOVE 1 TO END-OF-FILE
+           END-READ
+           
+      ******************************************************************
+           IF END-OF-FILE = 1
+            CLOSE VISIT_FILE
+           END-IF
+          
+           MOVE 0 TO END-OF-FILE.
+           OPEN OUTPUT TRANSACTIONS
+
+           PERFORM UNTIL END-OF-FILE = 1
+              
+                MOVE  INPUT-RECORD TO OUTPUT_RECCORD
+                UNSTRING INPUT-RECORD DELIMITED "." INTO 
+                IP_1 IP_2 IP_3 IP_4
+                DISPLAY  IP_1 "*" IP_2 "*" IP_3 "* " IP_4
+
+
+                WRITE OUTPUT_RECCORD 
+                READ VISIT_FILE
+                AT END MOVE 1 TO END-OF-FILE
+                END-READ
+           END-PERFORM
+
+               
+               
+           CLOSE TRANSACTIONS.
+
+          
+
+
+
+        000-TRT-FONC001-FIN.
+           EXIT. 
