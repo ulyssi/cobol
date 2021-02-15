f936c31950193521bad9eee9cae8271739f35d24
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 23:44:34 +0100

    new version

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -1,10 +1,6 @@
-#mkdir src
-
-#docker build
-#docker run --rm -it -v `pwd`/src:/oscobol/src:ro --name oscobol 
-
 
 rm src/*.txt
+rm src/.htpasswd
 
 echo "============================== GENERATION DE LA DONNEE======================"
 m_month=`date +'%b'`
@@ -21,14 +17,24 @@ docker build  . -t batch_cobol_run
 docker run  --rm -v `pwd`/src:/oscobol/src:rw --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/BATCH_1.cbl;cobcrun BATCH_1"
 
 echo "===============================EXPOSE DATA==================="
+
+
+rm expose/.htpasswd
+mkdir expose
+
+cp -rv src expose
+
 docker stop  nginx_expose_data
 docker rm nginx_expose_data
-docker run --name nginx_expose_data -p 8585:80  -v  `pwd`/src:/usr/share/nginx/html -d nginx
+
+
+docker run --name nginx_expose_data -p 8585:80  -v  `pwd`/expose:/usr/share/nginx/html -d nginx
+docker exec  nginx_expose_data bash -c 'sed  -i "s/index  index.html index.htm;/autoindex on;/g" /etc/nginx/conf.d/default.conf '
+docker exec  nginx_expose_data bash -c 'nginx -s reload '
 
 ##Protection des données.
-#echo "aaa:password ?" >> src/.htpasswd
-#docker exec  nginx_expose_data bash -c 'sed  -i "s/index  index.html index.htm;/autoindex on;auth_basic   \"closed site\";auth_basic_user_file \/usr\/share\/nginx\/html\/.htpasswd;/g" /etc/nginx/conf.d/default.conf '
-#docker exec  nginx_expose_data bash -c 'sed  -i "s/location \/ {/location \/data {/g" /etc/nginx/conf.d/default.conf '
-#docker exec  nginx_expose_data bash -c 'nginx -s reload '
 
-
+echo "tom:{SHA}loNd2L+nGL1kR8zIevia4Wddrso=" >  expose/.htpasswd
+docker exec  nginx_expose_data bash -c 'sed  -i "s/autoindex on;/autoindex on;auth_basic   \"closed site\";auth_basic_user_file \/usr\/share\/nginx\/html\/.htpasswd;/g" /etc/nginx/conf.d/default.conf '
+docker exec  nginx_expose_data bash -c 'sed  -i "s/location \/ {/location \/src {/g" /etc/nginx/conf.d/default.conf '
+docker exec  nginx_expose_data bash -c 'nginx -s reload '
