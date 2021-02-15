999f673a404c859bd738797b738c7a4e6fb44ba4
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 16:14:05 +0100

    batc

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -3,4 +3,4 @@
 #docker build
 #docker run --rm -it -v `pwd`/src:/oscobol/src:ro --name oscobol 
 docker build  . -t batch_cobol_run
-docker run  --rm -v `pwd`/src:/oscobol/src:ro --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/batch_1.cbl;cobcrun batch_1"
+docker run  --rm -v `pwd`/src:/oscobol/src:ro --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/BATCH_1.cbl;cobcrun BATCH_1"
