e41e62b2a5764d45ac31236073fcede6d9fc3a30
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 21:44:23 +0100

    destroy transaction

--- src/BATCH_1.cbl
+++ src/BATCH_1.cbl
@@ -25,26 +25,6 @@
        FD TRANSACTIONS.
        01 OUTPUT_RECCORD     PIC X(50).
        
-       01 TRANSACTION-STRUCT.
-         02 UID PIC 9(5).
-         02 DESC PIC X(25).
-         02 DETAILS.
-           03 AMOUNT PIC 9(6)V9(2).
-           03 START-BALANCE PIC 9(6)V9(2).
-           03 END-BALANCE PIC 9(6)V9(2).
-         02 ACCOUNT-ID PIC 9(7).
-         02 ACCOUNT-HOLDER PIC A(50).
-         02 WS-CURRENT-DATE-TRANSACTION-ID.
-           05  WS-CURRENT-DATE.
-              10  WS-CURRENT-YEAR         PIC 9(04).
-              10  WS-CURRENT-MONTH        PIC 9(02).
-              10  WS-CURRENT-DAY          PIC 9(02).
-           05 WS-CURRENT-TIME.
-              10  WS-CURRENT-HOURS        PIC 9(02).
-              10  WS-CURRENT-MINUTE       PIC 9(02).
-              10  WS-CURRENT-SECOND       PIC 9(02).
-              10  WS-CURRENT-MILLISECONDS PIC 9(02).       
-
 
        FD VISIT_FILE.
         01 INPUT-RECORD    PIC X(50).
