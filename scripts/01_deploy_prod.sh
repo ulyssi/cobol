1112208088a1621bb648fc7e83150886a8e3a179
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 20:46:43 +0100

    err

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -10,6 +10,5 @@ m_month=`date +'%b'`
 docker logs wordpress_cca_wordpress_1  >test  2>/dev/null ; cat test | grep "/${m_month}/"| grep '\- \-' | cut -d':' -f1> src/data.txt
 echo "" >> src/data.txt
 sed -i ':a;N;$!ba;s/\n/]\n/g ' src/data.txt
-cat src/data.txt
 docker run  --rm -v `pwd`/src:/oscobol/src:rw --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/BATCH_1.cbl;cobcrun BATCH_1"
 cat src/transactions.txt
