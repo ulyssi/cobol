225e728c3919e976c50c5126bd7df404d428d712
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-15 21:36:35 +0100

    src

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -10,12 +10,10 @@ m_month=`date +'%b'`
 docker logs wordpress_cca_wordpress_1  >test  2>/dev/null ; cat test | grep "/${m_month}/"| grep '\- \-' | cut -d':' -f1> src/data.txt
 echo "" >> src/data.txt
 sed -i ':a;N;$!ba;s/\n/]\n/g ' src/data.txt
-LONGEUR=50
 awk '{printf("%-'${LONGUEUR}'s\n", $0) }' src/data.txt > src/data2.txt
-
+mv  src/data2.txt src/data.txt
 
 
 echo " RUNNING COBOL"
 docker build  . -t batch_cobol_run
 docker run  --rm -v `pwd`/src:/oscobol/src:rw --name batch_cobol_run  --entrypoint /bin/bash batch_cobol_run  -c "cobc /oscobol/src/BATCH_1.cbl;cobcrun BATCH_1"
-cat src/transactions.txt
