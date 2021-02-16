5a8361e1fb0189d562ef88b25763944c5354f694
CADOUR Ulysse <ulysse.cadour@infotel.com>
2021-02-16 01:23:04 +0100

    docker

--- scripts/01_deploy_prod.sh
+++ scripts/01_deploy_prod.sh
@@ -5,7 +5,6 @@ rm src/.htpasswd
 echo "============================== GENERATION DE LA DONNEE======================"
 m_month=`date +'%b'`
 docker logs wordpress_cca_wordpress_1  >test  2>/dev/null ; cat test | grep "/${m_month}/"| grep '\- \-' | cut -d':' -f1> src/data.txt
-echo "" >> src/data.txt
 sed -i ':a;N;$!ba;s/\n/]\n/g ' src/data.txt
 LONGUEUR=50 
 awk '{printf("%-'${LONGUEUR}'s\n", $0) }' src/data.txt > src/data2.txt
@@ -19,22 +18,16 @@ docker run  --rm -v `pwd`/src:/oscobol/src:rw --name batch_cobol_run  --entrypoi
 echo "===============================EXPOSE DATA==================="
 
 
-# rm expose/.htpasswd
-# mkdir expose
+mkdir expose
+cp -rv src expose
+docker stop  nginx_expose_data
+docker rm nginx_expose_data
 
-# cp -rv src expose
-
-# docker stop  nginx_expose_data
-# docker rm nginx_expose_data
-
-
-# docker run --name nginx_expose_data -p 8585:80  -v  `pwd`/expose:/usr/share/nginx/html -d nginx
-# docker exec  nginx_expose_data bash -c 'sed  -i "s/index  index.html index.htm;/autoindex on;/g" /etc/nginx/conf.d/default.conf '
-# docker exec  nginx_expose_data bash -c 'nginx -s reload '
-
-# ##Protection des données.
-
-# echo "tom:{SHA}loNd2L+nGL1kR8zIevia4Wddrso=" >  expose/.htpasswd
-# docker exec  nginx_expose_data bash -c 'sed  -i "s/autoindex on;/autoindex on;auth_basic   \"closed site\";auth_basic_user_file \/usr\/share\/nginx\/html\/.htpasswd;/g" /etc/nginx/conf.d/default.conf '
-# docker exec  nginx_expose_data bash -c 'sed  -i "s/location \/ {/location \/src {/g" /etc/nginx/conf.d/default.conf '
-# docker exec  nginx_expose_data bash -c 'nginx -s reload '
+docker run --name nginx_expose_data -p 8585:80  -v  `pwd`/expose:/usr/share/nginx/html -d nginx
+docker exec  nginx_expose_data bash -c 'sed  -i "s/index  index.html index.htm;/autoindex on;/g" /etc/nginx/conf.d/default.conf '
+docker exec  nginx_expose_data bash -c 'nginx -s reload '
+ ##Protection des données.
+echo "tom:{SHA}loNd2L+nGL1kR8zIevia4Wddrso=" >  expose/.htpasswd
+docker exec  nginx_expose_data bash -c 'sed  -i "s/autoindex on;/autoindex on;auth_basic   \"closed site\";auth_basic_user_file \/usr\/share\/nginx\/html\/.htpasswd;/g" /etc/nginx/conf.d/default.conf '
+docker exec  nginx_expose_data bash -c 'sed  -i "s/location \/ {/location \/src {/g" /etc/nginx/conf.d/default.conf '
+docker exec  nginx_expose_data bash -c 'nginx -s reload '
